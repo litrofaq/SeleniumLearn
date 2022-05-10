c7819977e296d3d9d16f77e99a915539c6283435
litrofaq <litrofaq@yandex.ru>
2022-05-10 09:46:41 +0500

    Add Chromedriver, Nunit, Selenium

--- SeleniumLearn/SeleniumTests.cs
+++ SeleniumLearn/SeleniumTests.cs
@@ -1,5 +1,27 @@
-﻿namespace SeleniumLearn;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
-public class Class1
+namespace SeleniumLearn;
+
+public class SeleniumTests
 {
+    private ChromeDriver driver;
+    
+    [SetUp]
+    public void start()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+        driver = null;
+    }
 }
