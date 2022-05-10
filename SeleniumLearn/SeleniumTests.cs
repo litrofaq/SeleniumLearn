d8fd6149587252893679fe69da5490ae1cf1713b
litrofaq <litrofaq@yandex.ru>
2022-05-10 13:00:54 +0500

    Add Firefox Driver

--- SeleniumLearn/SeleniumTests.cs
+++ SeleniumLearn/SeleniumTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumLearn;
@@ -8,6 +9,7 @@ namespace SeleniumLearn;
 public class SeleniumTests
 {
     private ChromeDriver driver;
+    private FirefoxDriver firefoxDriver;
     
     [SetUp]
     public void start()
@@ -15,13 +17,21 @@ public class SeleniumTests
         var options = new ChromeOptions();
         options.AddArgument("start-maximized");
         driver = new ChromeDriver(options);
+        firefoxDriver = new FirefoxDriver();
     }
 
     [Test]
-    public void ConnectTest()
+    public void ChromeConnectTest()
     {
         driver.Url = "http://google.com/";
         driver.FindElement(By.Name("q")).SendKeys("WebDriver" + Keys.Enter);
+    }
+
+    [Test]
+    public void FirefoxConnectTest()
+    {
+        firefoxDriver.Url = "http://yandex.ru/";
+        firefoxDriver.FindElement(By.ClassName("input__control")).SendKeys("WebDriver" + Keys.Enter);
     }
 
 
@@ -30,5 +40,7 @@ public class SeleniumTests
     {
         driver.Quit();
         driver = null;
+        firefoxDriver.Quit();
+        firefoxDriver = null;
     }
 }
